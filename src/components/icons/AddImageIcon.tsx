35491623e4c5ca09fe058910e9d3eb454b11f205
Tang Jia Hui <jiahui.tang@agmostudio.com>
2024-08-29 15:16:22 +0800

    after yarn build

--- src/components/icons/AddImageIcon.tsx
+++ src/components/icons/AddImageIcon.tsx
@@ -11,10 +11,11 @@ export const AddImageIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
     stroke-width="1.25"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="icon icon-tabler icons-tabler-outline icon-tabler-upload">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
-      <path d="M7 9l5 -5l5 5" />
-      <path d="M12 4l0 12" />
+    class="icon icon-tabler icons-tabler-outline icon-tabler-upload"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+    <path d="M7 9l5 -5l5 5" />
+    <path d="M12 4l0 12" />
   </svg>
 );
