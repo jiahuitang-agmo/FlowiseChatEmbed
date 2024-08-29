30e73c3c25f68a94e48345a4dd0a48df7634d85c
Tang Jia Hui <jiahui.tang@agmostudio.com>
2024-08-29 15:57:54 +0800

    changed default message

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -136,7 +136,7 @@ export type LeadsConfig = {
   successMessage?: string;
 };
 
-const defaultWelcomeMessage = 'Heyyy there! How can I help?';
+const defaultWelcomeMessage = 'Heyyy there! How can I help ya?';
 
 /*const sourceDocuments = [
     {
